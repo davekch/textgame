ad616a8010038a75a37abb90e538dcd07b84645e
davekch <dave-koch@web.de>
2019-05-17 20:55:19 +0200

    Revert "remove setup.py for now"
    This reverts commit af99f9b6fbf03edf35130703af6a1c9d0c3c0429.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='textgame',
+    version='0.1',
+    description='Library to build a text based adventure game',
+    author='David Koch',
+    author_email='dave-koch@web.de',
+    packages=['textgame']
+)
