4401fba9fd9d2fef20af0fbb8bacbe8a50e1d75f
Dave Kch <dave-koch@web.de>
2019-06-10 14:13:16 +0200

    v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='textgame',
-    version='0.1',
+    version='0.2',
     description='Library to build a text based adventure game',
     author='David Koch',
     author_email='dave-koch@web.de',
