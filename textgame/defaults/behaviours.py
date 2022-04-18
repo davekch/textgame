ce9f6a804dacbf64958d8cd3bbe264ebdc193dc9
davekch <dave-koch@web.de>
2022-04-18 17:32:18 +0200

    fix randomappearance

--- textgame/defaults/behaviours.py
+++ textgame/defaults/behaviours.py
@@ -11,15 +11,14 @@ def randomappearance(creature: Creature, state: State, probability: float, rooms
     """
     spawns a creature in the same place as the player, but it vanishes after one step
     """
-    logger.debug(f"calling the singleencounterbehaviour of {creature.id!r}")
+    logger.debug(f"calling the randomappearance of {creature.id!r}")
     # check if the creature is in a room with the player
     if creature.id in state.player_location.creatures:
-        state.player_location.pop_creature(creature.id)
-        state.get_room("storage_room").add_creature(creature)
+        # put the creature inside the storage room
+        state.get_room("storage_room").creatures.add(creature)
     elif (
         creature.alive
         and state.random.random() < probability
         and any(r in state.player_location.id for r in rooms)
     ):
-        state.get_room("storage_room").pop_creature(creature.id)
-        state.player_location.add_creature(creature)
+        state.player_location.creatures.add(creature)
